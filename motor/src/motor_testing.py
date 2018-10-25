4fb207a73dd50621d458dc09f9adbc41afaacdd0
rickyc106 <rickyc106@gmail.com>
2018-10-24 20:31:21 -0500

    Add Base File Structure

---
+++ motor/src/motor_testing.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+def xbox_cb():
+	# Work in progress
+
+def publisher():
+	print(testing)
+
+if __name__ == '__main__':
+	try:
+		publisher()
+	except rospy.ROSInterruptException:
+		pass
+
+
