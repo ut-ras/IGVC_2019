74ce366672ebea7cfc28bde56b5138c4efd6c5c4
rickyc106 <rickyc106@gmail.com>
2018-11-05 16:54:13 -0600

    Add Unit Test Cases for EKF_main

--- lidar/src/ekf-slam/test_ekf_correction.py
+++ lidar/src/ekf-slam/test_ekf_correction.py
@@ -19,7 +19,7 @@ class EKF_CorrectionTest(unittest.TestCase):
 		test_array = array([174.9285568,1.030376827])
 		output = ekf_correction.h(self.state, self.landmark, self.scannerDisplacement)
 		self.assertTrue(allclose(output, test_array),
-					    'h did not equal test:\n\nOutput:\n{}\n\nTest:\n{}'\
+					    'h did not equal test:\n\nOutput:\n{}\n\nTest:\n{}\n'\
 					    .format(output, test_array))
 
 	def test_dh_dstate(self):
@@ -27,7 +27,7 @@ class EKF_CorrectionTest(unittest.TestCase):
 							 [0.0049019608,-0.0029411765,-1.029411765]])
 		output = ekf_correction.dh_dstate(self.state, self.landmark, self.scannerDisplacement)
 		self.assertTrue(allclose(output, test_matrix),
-					    'dh_dstate did not equal test:\n\nOutput:\n{}\n\nTest:\n{}'\
+					    'dh_dstate did not equal test:\n\nOutput:\n{}\n\nTest:\n{}\n'\
 					    .format(output, test_matrix))
 
 if __name__ == '__main__':
---
+++ lidar/src/ekf-slam/test_ekf_main.py
@@ -0,0 +1,121 @@
+#!/usr/bin/env python
+
+PKG = 'lidar'
+
+import rospy
+import unittest
+import ekf_main
+from numpy import *
+
+class EKF_MainTest(unittest.TestCase):
+	@classmethod
+	def setUpClass(self):
+		self.state = [100.0, 100.0, 0.0]
+		self.covariance = zeros([3,3])
+		self.width = 150.0
+		self.scannerDisplacement = 10.0
+		self.control_motion_factor = 0.35
+		self.control_turn_factor = 0.6
+		self.measurement_distance_stddev = 600.0
+		self.measurement_angle_stddev = 0.7854
+
+		self.equalControl = [10.0,10.0]
+		self.notEqualControl = [10.0, 15.0]
+
+		self.measurement = [200.0, 250.0]
+		self.no_landmarks = -1
+		self.first_landmark = 0
+
+		self.landmark_coords = self.measurement
+
+	@staticmethod
+	def reinitialize_object(self):
+		ekf = ekf_main.ExtendedKalmanFilter(self.state, self.covariance, self.width, self.scannerDisplacement,
+								   self.control_motion_factor, self.control_turn_factor,
+								   self.measurement_distance_stddev, self.measurement_angle_stddev)
+
+		return ekf
+
+	def test_predict(self):
+		ekf = EKF_MainTest.reinitialize_object(self)
+
+		test_state = array([110,100,0.0])
+		test_covariance = array([1,2,3]) # <-- Replace with test case
+
+		ekf.predict(self.equalControl)
+
+		self.assertTrue(allclose(ekf.state, test_state),
+						'Predicted State (l=r) did not equal test state:\n\nOutput:\n{}\n\nTest:\n{}\n'\
+						.format(ekf.state, test_state))
+
+		self.assertTrue(allclose(ekf.covariance, test_covariance),
+						'Predicted Covariance (l=r) did not equal test covariance:\n\nOutput:\n{}\n\nTest:\n{}\n'\
+						.format(ekf.covariance, test_covariance))
+
+		ekf = EKF_MainTest.reinitialize_object(self)
+
+		test_state = array([112.49768531,100.2083140439,0.033333333])
+		test_covariance = array([1,2,3]) # <-- Replace with test case
+
+		ekf.predict(self.notEqualControl)
+
+		self.assertTrue(allclose(ekf.state, test_state),
+						'Predicted State (l!=r) did not equal test state:\n\nOutput:\n{}\n\nTest:\n{}\n'\
+						.format(ekf.state, test_state))
+
+		self.assertTrue(allclose(ekf.covariance, test_covariance),
+						'Predicted Covariance (l!=r) did not equal test covariance:\n\nOutput:\n{}\n\nTest:\n{}\n'\
+						.format(ekf.covariance, test_covariance))
+
+	def test_add_landmark(self):
+		ekf = EKF_MainTest.reinitialize_object(self)
+
+		test_state = array([110,100,0.0]) # <-- Replace with test case
+		test_covariance = array([1,2,3]) # <-- Replace with test case
+		test_landmark_index = 0 # <-- Replace with test cas
+
+		ekf.add_landmark(self.landmark_coords)
+
+		self.assertTrue(allclose(ekf.state, test_state),
+						'Updated State with Landmark did not equal test state:\n\nOutput:\n{}\n\nTest:\n{}\n'\
+						.format(ekf.state, test_state))
+
+		self.assertTrue(allclose(ekf.covariance, test_covariance),
+						'Updated Covariance with Landmark did not equal test covariance:\n\nOutput:\n{}\n\nTest:\n{}\n'\
+						.format(ekf.covariance, test_covariance))
+
+		self.assertTrue(allclose(ekf.landmark_index, test_landmark_index),
+						'Updated landmark index did not equal test landmark index:\n\nOutput:\n{}\n\nTest:\n{}\n'\
+						.format(ekf.landmark_index, test_landmark_index))
+
+	def test_correct(self):
+		ekf = EKF_MainTest.reinitialize_object(self)
+
+		test_state = array([110,100,0.0]) # <-- Replace with test case
+		test_covariance = array([1,2,3]) # <-- Replace with test case
+
+		ekf.correct(self.measurement, self.first_landmark)
+
+		self.assertTrue(allclose(ekf.state, test_state),
+						'Corrected State did not equal test state:\n\nOutput:\n{}\n\nTest:\n{}\n'\
+						.format(ekf.state, test_state))
+
+		self.assertTrue(allclose(ekf.covariance, test_covariance),
+						'Corrected Covariance did not equal test covariance:\n\nOutput:\n{}\n\nTest:\n{}\n'\
+						.format(ekf.covariance, test_covariance))
+
+	def test_visualize_landmark_error_ellipse(self):
+		pass # Replace later
+
+	def test_visualize_pose_error_ellipse(self):
+		ekf = EKF_MainTest.reinitialize_object(self)
+
+		test_eigenvals = [1,2,3] # <-- Replace with test case
+		test_eigenvects = [1,2,3] # <-- Replace with test case
+		test_angle = 20 # <-- Replace with test case
+
+if __name__ == '__main__':
+	unittest.main()
+	#import rostest
+	#rostest.rosrun(PKG, 'EKF_CorrectionTest', EKF_CorrectionTest)
+
