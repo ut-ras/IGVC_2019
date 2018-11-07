0210fcafa2b4a7530ac4cb8678142b748c3bd1a0
rickyc106 <rickyc106@gmail.com>
2018-11-07 12:38:04 -0600

    Appends to Static List of Landmarks

--- lidar/src/landmark_extraction.py
+++ lidar/src/landmark_extraction.py
@@ -9,17 +9,22 @@ positions in global X-Y coordinates
 import rospy
 from sensor_msgs.msg import LaserScan
 from std_msgs.msg import Float64MultiArray
-import itertools
+from math import sin, cos, radians, degrees, sqrt
 
 class LandmarkExtraction:
 	def __init__(self, minimum_valid_distance, depth_jump, 
-				 cylinder_offset, max_cylinder_distance):
+				 cylinder_offset, max_landmark_radius):
 		self.depth_jump = depth_jump
 		self.cylinder_offset = cylinder_offset
-		self.max_cylinder_distance = max_cylinder_distance
+		self.max_landmark_radius = max_landmark_radius
 		self.minimum_valid_distance = minimum_valid_distance
 
 		self.landmark_msg = Float64MultiArray()
+		self.print_x_y = True
+
+		self.current_landmark = []
+		self.output_landmark = []
+		self.landmark_index = -1
 
 	def flatten(self, list_of_lists):
 		new_list = []
@@ -31,10 +36,10 @@ class LandmarkExtraction:
 		return new_list
 
 	def laserScanCb(self, msg):
-		landmark_bearing, current_landmark, output_landmark = [], [], []
-		landmark_index = -1
-
+		new_landmark_found = False
 		left_edge_found = False
+
+		#print self.output_landmark
 
 		#print "start-------------------------------\n"
 		
@@ -46,7 +51,7 @@ class LandmarkExtraction:
 				continue
 
 			if (derivative < -self.depth_jump):
-				bearing = angle * 0.25 - 135 
+				bearing = angle * 0.25 - 135.0 
 				left_edge = bearing
 
 				#print "left_edge found at {}".format(left_edge)
@@ -54,28 +59,58 @@ class LandmarkExtraction:
 				left_edge_found = True
 
 			if (derivative > self.depth_jump):
-				bearing = angle * 0.25 - 135
+				bearing = angle * 0.25 - 135.0
 				right_edge = bearing
 				
 				#print "right_edge found at {}".format(right_edge)
 				
 				if left_edge_found:
-					landmark_bearing.append((right_edge + left_edge) / 2)
-					landmark_index += 1
+					landmark_bearing = (right_edge + left_edge) / 2
+					landmark_range = msg.ranges[int((landmark_bearing + 135.0) * 4.0)]
 
-					#self.landmark_msg.data = landmark_bearing
+					landmark_x = landmark_range * cos(radians(landmark_bearing))
+					landmark_y = landmark_range * sin(radians(landmark_bearing))
 
-					current_range = msg.ranges[int((landmark_bearing[landmark_index] + 135) * 4)]
-					current_bearing = landmark_bearing[landmark_index]
+					if self.output_landmark == []:
+						new_landmark_found = True
+						self.landmark_index += 1
+					else:
+						# Loop through all existing landmarks
+						# If no correspondence is found, set new_landmark_found flag to true
+						for index in range(self.landmark_index + 1):
+							distance_x = landmark_x - self.output_landmark[index][0]
+							distance_y = landmark_y - self.output_landmark[index][1]
 
-					output_landmark.append([current_range, current_bearing]) # 2D Array for Readability here
-					current_landmark = self.flatten(output_landmark) # Flatten to 1D for ROS
+							distance_to_landmark = sqrt(distance_x**2 + distance_y**2)
 
-					self.landmark_msg.data = current_landmark
+							if (distance_to_landmark < max_landmark_radius):
+								# Assign Correspondence here
+								new_landmark_found = False
+								break
+
+							if (distance_to_landmark > max_landmark_radius):
+								new_landmark_found = True
+
+						if new_landmark_found:
+							self.landmark_index += 1
+
+					# Print landmark (X,Y)
+					if self.print_x_y:
+						if new_landmark_found:
+							self.output_landmark.append([landmark_x, landmark_y]) # 2D Array for readability here
+					# Print landmark (Range, Bearing) instead
+					else:
+						if new_landmark_found:
+							self.output_landmark.append([landmark_range, landmark_bearing]) # 2D Array for readability here
+
+					self.current_landmark = self.flatten(self.output_landmark) # Flatten to 1D for ROS
+
+					self.landmark_msg.data = self.current_landmark
 
 					#print "landmark: {} found between bearing ({}) and ({})".format(current_landmark, left_edge, right_edge)
 
 				left_edge_found = False
+				new_landmark_found = False
 
 		#print "\nstop-------------------------------- \n"
 
@@ -98,10 +133,10 @@ if __name__ == '__main__':
 		minimum_valid_distance = rospy.get_param("/landmark_extraction/minimum_valid_distance")
 		depth_jump = rospy.get_param("/landmark_extraction/depth_jump")
 		cylinder_offset = rospy.get_param("/landmark_extraction/cylinder_offset")
-		max_cylinder_distance = rospy.get_param("/landmark_extraction/max_cylinder_distance")
+		max_landmark_radius = rospy.get_param("/landmark_extraction/max_landmark_radius")
 
 		extractor = LandmarkExtraction(minimum_valid_distance, depth_jump,
-									   cylinder_offset, max_cylinder_distance)
+									   cylinder_offset, max_landmark_radius)
 		extractor.main()
 
 	except rospy.ROSInterruptException:
