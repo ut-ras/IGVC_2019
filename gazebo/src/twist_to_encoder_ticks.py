dfd860cede9d778782d025d006891d6105f9722e
rickyc106 <rickyc106@gmail.com>
2019-06-01 01:10:16 -0500

    Add fake encoder ticks

---
+++ gazebo/src/twist_to_encoder_ticks.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+"""
+   twist_to_motors - converts a twist message to motor commands.  Needed for navigation stack
+   
+   
+	Copyright (C) 2012 Jon Stephan. 
+	Edited: Ricky Chen.
+	 
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+import rospy
+import roslib
+from std_msgs.msg import Float32MultiArray
+from geometry_msgs.msg import Twist 
+
+class TwistToMotors():
+	def __init__(self):
+		rospy.init_node("twist_to_motors")
+		nodename = rospy.get_name()
+		rospy.loginfo("%s started" % nodename)
+	
+		self.w = rospy.get_param("~base_width", 0.2)
+	
+		self.pub_encoder_ticks = rospy.Publisher('encoder_ticks', Float32MultiArray, queue_size=10)
+		rospy.Subscriber('cmd_vel', Twist, self.twistCallback)
+	
+		self.rate = rospy.get_param("~rate", 50)
+		self.timeout_ticks = rospy.get_param("~timeout_ticks", 2)
+		self.left = 0
+		self.right = 0
+
+		self.encoder_ticks = Float32MultiArray()
+
+	def spin(self):
+		r = rospy.Rate(self.rate)
+		idle = rospy.Rate(10)
+		then = rospy.Time.now()
+		self.ticks_since_target = self.timeout_ticks
+	
+		while not rospy.is_shutdown():
+			while not rospy.is_shutdown() and self.ticks_since_target < self.timeout_ticks:
+				self.spinOnce()
+				r.sleep()
+			idle.sleep()
+				
+	def spinOnce(self):
+		# dx = (l + r) / 2
+		# dr = (r - l) / w
+			
+		self.right = 1.0 * self.dx + self.dr * self.w / 2 
+		self.left = 1.0 * self.dx - self.dr * self.w / 2
+		# rospy.loginfo("publishing: (%d, %d)", left, right) 
+
+		self.encoder_ticks.data = [self.left, self.right]
+		self.pub_encoder_ticks.publish(self.encoder_ticks)
+			
+		self.ticks_since_target += 1
+
+	def twistCallback(self,msg):
+		# rospy.loginfo("-D- twistCallback: %s" % str(msg))
+		self.ticks_since_target = 0
+		self.dx = msg.linear.x
+		self.dr = msg.angular.z
+		self.dy = msg.linear.y
+
+if __name__ == '__main__':
+	""" main """
+	try:
+		twistToMotors = TwistToMotors()
+		twistToMotors.spin()
+	except rospy.ROSInterruptException:
+		pass
